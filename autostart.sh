d1f306a7434bfca2271226bd27067e9c665e793b
Conimbad <escocarl@gmail.com>
2022-01-16 19:12:56 -0600

    Add files via upload

--- autostart.sh
+++ autostart.sh
@@ -3,5 +3,16 @@
 sh /home/alastor/.screenlayout/resolution.sh &
 
 #Wallpaper
-nitrogen --restore
+nitrogen --restore &
 
+#pulse audio applet for the bar
+pa-applet & 
+
+# Mounting disks
+udiskie &
+
+# Notifications
+dunst &
+
+# lxsession systemtray
+lxsession &
